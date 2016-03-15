0bc9ed9751cf8e3a0d03813222d02da5be76cb68
cgeek <cem.moreau@gmail.com>
2016-03-15 18:30:43 +0100

    Fix: wrong require

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 module.exports = new function() {
 
   var binary = require('node-pre-gyp');
-  var path = require('naclb');
+  var path = require('path');
   var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
   var binding = require(binding_path);
 
